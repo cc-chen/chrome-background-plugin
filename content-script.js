10996930ec8a4514e8b0922763b4c0476f7b4df8
sunny <chyg2008@foxmail.com>
2020-09-23 16:30:59 +0800

    init

---
+++ content-script.js
@@ -0,0 +1,5 @@
+﻿(function() {
+	console.log('config background start！');
+	document.getElementsByTagName('body')[0].style.background='#C7EDCC';x=document.getElementsByTagName('div');for(var i=0;i<x.length;i++){x[i].style.background='#C7EDCC'}
+	console.log('config background over！');
+})();
